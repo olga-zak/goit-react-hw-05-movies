df1c58ab02ae470880dcd4467af13a865221aea6
olga-zak <olgaliam@gmail.com>
2022-12-18 16:57:54 +0200

    stylize MovieCard

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -13,61 +13,98 @@ import {
   Background,
   AdditionalInfo,
   AdditionalInfoItem,
+  ListItem,
 } from './MovieCard.styled';
 
 export const MovieCard = ({ movieData }) => {
-  //console.log(movieData);
-  const { title, release_date, poster_path, overview, genres, vote_average } =
-    movieData;
+  console.log(movieData);
+  const {
+    title,
+    release_date,
+    poster_path,
+    overview,
+    genres,
+    vote_average,
+    backdrop_path,
+  } = movieData;
   const listOfGenres = genres.map(genre => (
-    <li key={genre.id}>{genre.name}</li>
+    <ListItem key={genre.id}>{genre.name}</ListItem>
   ));
   const userScore = Math.round(vote_average * 10);
+
   return (
     <MovieCardWrapper>
-      <InfoSection>
-        <MovieHeader>
-          <Image
-            src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-            alt={title}
-          />
-          <MainHeader>{title}</MainHeader>
-          <SubHeader>{release_date}</SubHeader>
-          <Score>Score {userScore}%</Score>
-          <Genres>{listOfGenres}</Genres>
-        </MovieHeader>
+      <MovieHeader>
+        <Image
+          src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+          alt={title}
+        />
+        <MainHeader>
+          {title} ({release_date})
+        </MainHeader>
+        <Score>Score {userScore}%</Score>
+        <Genres>{listOfGenres}</Genres>
+      </MovieHeader>
 
-        <Overview>{overview}</Overview>
+      <Overview>{overview}</Overview>
 
-        <AdditionalInfo>
-          <ul>
-            <AdditionalInfoItem>
-              <Link to="cast">Cast</Link>
-            </AdditionalInfoItem>
-            <AdditionalInfoItem>
-              <Link to="reviews">Reviews</Link>
-            </AdditionalInfoItem>
-          </ul>
-        </AdditionalInfo>
-      </InfoSection>
-      <Background></Background>
+      <AdditionalInfo>
+        <AdditionalInfoItem>
+          <Link to="cast">Cast</Link>
+        </AdditionalInfoItem>
+        <AdditionalInfoItem>
+          <Link to="reviews">Reviews</Link>
+        </AdditionalInfoItem>
+      </AdditionalInfo>
+
+      <Background
+        someUrl={`https://image.tmdb.org/t/p/w500${backdrop_path}`}
+      ></Background>
     </MovieCardWrapper>
-    // <>
-    //   <p>This is movie card</p>
-    //   <h1>
-    //     {title} ({release_date})
-    //   </h1>
-    //   <p>User Score: {userScore}%</p>
-    //   <h2>Overview</h2>
-    //   <p>{overview}</p>
-    //   <h3>Genres</h3>
-    //   <ul>{listOfGenres}</ul>
-    //   <img
-    //     src={`https://image.tmdb.org/t/p/w500${poster_path}`}
-    //     alt="{title}"
-    //     width="200px"
-    //   />
-    // </>
+    // <MovieCardWrapper>
+    //   <InfoSection>
+    //     <MovieHeader>
+    //       <Image
+    //         src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+    //         alt={title}
+    //       />
+    //       <MainHeader>{title}</MainHeader>
+    //       <SubHeader>{release_date}</SubHeader>
+    //       <Score>Score {userScore}%</Score>
+    //       <Genres>{listOfGenres}</Genres>
+    //     </MovieHeader>
+
+    //     <Overview>{overview}</Overview>
+
+    //     <AdditionalInfo>
+    //       <ul>
+    //         <AdditionalInfoItem>
+    //           <Link to="cast">Cast</Link>
+    //         </AdditionalInfoItem>
+    //         <AdditionalInfoItem>
+    //           <Link to="reviews">Reviews</Link>
+    //         </AdditionalInfoItem>
+    //       </ul>
+    //     </AdditionalInfo>
+    //   </InfoSection>
+    //   <Background></Background>
+    // </MovieCardWrapper>
+    // // <>
+    // //   <p>This is movie card</p>
+    // //   <h1>
+    // //     {title} ({release_date})
+    // //   </h1>
+    // //   <p>User Score: {userScore}%</p>
+    // //   <h2>Overview</h2>
+    // //   <p>{overview}</p>
+    // //   <h3>Genres</h3>
+    // //   <ul>{listOfGenres}</ul>
+    // //   <img
+    // //     src={`https://image.tmdb.org/t/p/w500${poster_path}`}
+    // //     alt="{title}"
+    // //     width="200px"
+    // //   />
+    // // </>
   );
 };
 
--- src/components/MovieCard/MovieCard.styled.js
+++ src/components/MovieCard/MovieCard.styled.js
@@ -4,19 +4,13 @@ import styled from 'styled-components';
 //may be transition is not needed?
 export const MovieCardWrapper = styled.div`
   position: relative;
-  display: block;
+  //display: block;
   width: 800px;
-  height: 350px;
+  min-height: 250px;
   margin: 20px auto;
-  overflow: hidden;
+  //overflow: hidden;
   border-radius: 10px;
-  //transition: all 0.4s;
   box-shadow: 0px 0px 120px -25px rgba(0, 0, 0, 0.5);
-  /* &:hover {
-    transform: scale(1.02);
-    box-shadow: 0px 0px 80px -25px rgba(0, 0, 0, 0.5);
-    transition: all 0.4s;
-  } */
 `;
 
 export const InfoSection = styled.div`
@@ -29,12 +23,13 @@ export const InfoSection = styled.div`
 `;
 
 export const MovieHeader = styled.div`
-  position: relative;
+  //position: relative;
   padding: 25px;
   height: 40%;
 `;
 
 export const MainHeader = styled.h1`
+  margin-bottom: 15px;
   color: black;
   font-weight: 400;
 `;
@@ -45,16 +40,16 @@ export const SubHeader = styled.h4`
 `;
 
 export const Score = styled.span`
-  display: inline-block;
+  /* display: inline-block;
   margin-top: 15px;
   color: #555;
   padding: 5px;
   border-radius: 5px;
-  border: 1px solid rgba(0, 0, 0, 0.05);
+  border: 1px solid rgba(0, 0, 0, 0.05); */
 `;
 
 export const Image = styled.img`
-  position: relative;
+  //position: relative;
   float: left;
   margin-right: 20px;
   height: 120px;
@@ -62,55 +57,62 @@ export const Image = styled.img`
 `;
 
 export const Genres = styled.ul`
-  display: inline-block;
-  color: #959595;
-  margin-left: 10px;
+  display: flex;
+  margin-top: 15px;
+  color: #141313;
+`;
+export const ListItem = styled.li`
+  margin-right: 5px;
+  &:not(:last-child):after {
+    content: ',';
+  }
 `;
 
 export const Overview = styled.p`
   padding: 25px;
   height: 50%;
-  color: #545454;
+  color: black;
 `;
 
 export const Background = styled.div`
   position: absolute;
   top: 0;
-  z-index: 1;
+  z-index: -1;
+
   height: 100%;
   width: 100%;
   right: 0;
   background-size: cover;
   border-radius: 11px;
 
-  background: #ada996; /* fallback for old browsers */
-  background: -webkit-linear-gradient(
-    to right,
-    #eaeaea,
-    #dbdbdb,
-    #f2f2f2,
-    #ada996
-  ); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to right,
-    #eaeaea,
-    #dbdbdb,
-    #f2f2f2,
-    #ada996
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+  background-image: url(${props => props.someUrl});
+  background-repeat: no-repeat;
+  opacity: 0.3;
+  background-position: 50% 50%;
+  // background: #ada996; /* fallback for old browsers */
+  // background: -webkit-linear-gradient(
+  //   to right,
+  //   #eaeaea,
+  //   #dbdbdb,
+  //   #f2f2f2,
+  //   #ada996
+  // ); /* Chrome 10-25, Safari 5.1-6 */
+  // background: linear-gradient(
+  //   to right,
+  //   #eaeaea,
+  //   #dbdbdb,
+  //   #f2f2f2,
+  //   #ada996
+  // ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-  /* background-image: radial-gradient(
-    circle at top right,
-    var(--start-color) 10%,
-    var(--end-color) 50%
-  ); */
   //background-image: url('https://occ-0-2433-448.1.nflxso.net/art/cd5c9/3e192edf2027c536e25bb5d3b6ac93ced77cd5c9.jpg');
 `;
 
-export const AdditionalInfo = styled.div`
+export const AdditionalInfo = styled.ul`
   height: 10%;
-  padding-left: 15px;
-  padding-bottom: 20px;
+  //padding-left: 15px;
+  //padding-bottom: 20px;
+  padding: 25px;
 `;
 export const AdditionalInfoItem = styled.li`
   display: inline-block;
