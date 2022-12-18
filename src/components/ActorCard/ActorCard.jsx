f58d35b97e18b033ebbdc7b8fa55b313a9836e52
olga-zak <olgaliam@gmail.com>
2022-12-18 21:59:12 +0200

    stylize ActorCard

--- src/components/ActorCard/ActorCard.jsx
+++ src/components/ActorCard/ActorCard.jsx
@@ -1,11 +1,14 @@
+import { CardWrapper, Img, Name, Role } from './ActorCard.styled';
+
 export const ActorCard = ({ name, role, photo_path }) => {
   return (
     <>
-      <p>this is actor card</p>
-      <p>{name}</p>
-      <p>{role}</p>
-
-      <img src={`https://image.tmdb.org/t/p/w500${photo_path}`} alt={name} />
+      <CardWrapper>
+        <Img src={`https://image.tmdb.org/t/p/w500${photo_path}`} alt={name} />
+        <Name>{name}</Name>
+        <Role>{role}</Role>
+        {/* <p className={styles.location}>{location}</p> */}
+      </CardWrapper>
     </>
   );
 };
---
+++ src/components/ActorCard/ActorCard.styled.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const CardWrapper = styled.div`
+  width: 240px;
+  margin: 0 auto;
+  padding: 30px 0;
+  text-align: center;
+  background-color: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+`;
+
+export const Img = styled.img`
+  width: 100px;
+  margin: 0 auto 25px auto;
+  border-radius: 50%;
+`;
+
+export const Name = styled.p`
+  margin-bottom: 10px;
+  font-size: 20px;
+  color: black;
+  font-weight: bold;
+`;
+
+export const Role = styled.p`
+  font-size: 15px;
+  font-weight: 500;
+`;
